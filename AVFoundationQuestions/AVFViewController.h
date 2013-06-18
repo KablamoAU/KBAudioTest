2539638aa2720b0cc17de01f00277499c0ae9b44
jusapo <justin@kablamo.com.au>
2013-06-18 17:12:23 +1000

    allans mystery changes

--- AVFoundationQuestions/AVFViewController.h
+++ AVFoundationQuestions/AVFViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface AVFViewController : UIViewController
-
+{
+    UIBackgroundTaskIdentifier task;
+}
 @end
--- AVFoundationQuestions/AVFViewController.m
+++ AVFoundationQuestions/AVFViewController.m
@@ -17,6 +17,7 @@ void *kDidFailKVO               = &kDidFailKVO;
     PlayerView *playerView;
     id timeObserver;
     NSInteger _playing;
+    AVPlayerItem *playerItem;
 }
 @end
 
@@ -26,6 +27,7 @@ void *kDidFailKVO               = &kDidFailKVO;
 {
     [super viewDidLoad];
     [self playURL:[NSURL URLWithString:@"http://radionetworknz-ice.streamguys.com/zmonline"]];
+    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
 }
 
 - (void)viewDidUnload
@@ -48,10 +50,18 @@ void *kDidFailKVO               = &kDidFailKVO;
 
 #pragma -
 
+- (void)playerItemDidReachEnd:(NSNotification *)notification {
+//    AVPlayerItem *p = [notification object];
+//    [p seekToTime:kCMTimeZero];
+    NSLog(@"PlayerITemDidReachEnd");
+    player = nil;
+    [self playURL:[NSURL URLWithString:@"http://radionetworknz-ice.streamguys.com/zmonline"]];
+}
+
 - (void)playURL:(NSURL *)videoURL
 {
     if (!player) {
-        player = [[AVPlayer alloc] init];
+        player = [[AVPlayer alloc] init];        
         playerView = [[PlayerView alloc] init];
         [playerView setPlayer:player];
 //        playerView.frame = CGRectInset(self.view.bounds, 20, 20);
@@ -70,13 +80,24 @@ void *kDidFailKVO               = &kDidFailKVO;
         [player addObserver:self forKeyPath:@"currentItem.playbackBufferFull"  options:NSKeyValueObservingOptionNew context:kBufferFullKVO];    
         [player addObserver:self forKeyPath:@"currentItem.playbackBufferEmpty" options:NSKeyValueObservingOptionNew context:kBufferEmptyKVO];    
         [player addObserver:self forKeyPath:@"currentItem.error"               options:NSKeyValueObservingOptionNew context:kDidFailKVO];    
-        
+//        [player addObserver:self forKeyPath:@"audioPlayerDidFinishPlaying:successfully:"]
     }
-    [player replaceCurrentItemWithPlayerItem:[[AVPlayerItem alloc] initWithURL:videoURL]];
-    
+    playerItem = [[AVPlayerItem alloc] initWithURL:videoURL];
+    [playerItem addObserver:self forKeyPath:@"playbackBufferEmpty" options:NSKeyValueObservingOptionNew context:nil];
+    [playerItem addObserver:self forKeyPath:@"playbackLikelyToKeepUp" options:NSKeyValueObservingOptionNew context:nil];
+    [player replaceCurrentItemWithPlayerItem:playerItem];
+    player.actionAtItemEnd = AVPlayerActionAtItemEndNone;
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playerItemDidReachEnd:)
+                                                 name:AVPlayerItemDidPlayToEndTimeNotification
+                                               object:[player currentItem]];
     timeObserver = [player addPeriodicTimeObserverForInterval:CMTimeMakeWithSeconds(0.5, 600) queue:nil usingBlock:^(CMTime time) {
         NSLog(@"Playback time %.5f", CMTimeGetSeconds(time));
     }];
+}
+
+- (void)audioPlayerDidFinishPlaying
+{
+    NSLog(@"AudioPlayerDidFinishPlaying");
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context 
@@ -99,23 +120,48 @@ void *kDidFailKVO               = &kDidFailKVO;
         if (timeRanges && [timeRanges count]) {
             CMTimeRange timerange = [[timeRanges objectAtIndex:0] CMTimeRangeValue];
             NSLog(@" . . . %.5f -> %.5f", CMTimeGetSeconds(timerange.start), CMTimeGetSeconds(CMTimeAdd(timerange.start, timerange.duration)));
-            if (CMTIME_COMPARE_INLINE(timerange.duration, >, CMTimeMakeWithSeconds(10, timerange.duration.timescale))) {
+            if (CMTIME_COMPARE_INLINE(timerange.duration, >, CMTimeMakeWithSeconds(3, timerange.duration.timescale))) {
                 if (!_playing) {
                     [player play];
                     _playing = 1;
                 }
             } 
-            if (CMTIME_COMPARE_INLINE(timerange.duration, >, CMTimeMakeWithSeconds(15, timerange.duration.timescale))) {
+            if (CMTIME_COMPARE_INLINE(timerange.duration, >, CMTimeMakeWithSeconds(10, timerange.duration.timescale))) {
                 if (_playing == 1) {
-                   // [player pause];
-                    _playing = 2;
+                   [player play];
+                    _playing = 1;
                 }
             } 
             if (CMTIME_COMPARE_INLINE(timerange.duration, >=, CMTimeMakeWithSeconds(20, timerange.duration.timescale))) {
                 if (_playing == 2) {
                     [player play];
-                    _playing = 3;
+//                    _playing = 3;
                 }
+            }
+        }
+    }
+    else if ([keyPath isEqualToString:@"playbackBufferEmpty"])
+    {
+        if (playerItem.playbackBufferEmpty) {
+            [[NSNotificationCenter defaultCenter] postNotificationName:@"message" object:@"Buffering..."];
+            
+            if([[UIApplication sharedApplication] applicationState] == UIApplicationStateBackground)
+            {
+                task = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^(void) {
+                }];
+            }
+        }
+    }
+    else if ([keyPath isEqualToString:@"playbackLikelyToKeepUp"])
+    {
+        if (playerItem.playbackLikelyToKeepUp)
+        {
+            [player play];
+            
+            if([[UIApplication sharedApplication] applicationState] == UIApplicationStateBackground)
+            {
+                [[UIApplication sharedApplication] endBackgroundTask:task];
+                task = 0;
             }
         }
     }
@@ -123,5 +169,22 @@ void *kDidFailKVO               = &kDidFailKVO;
     //NSLog(@"%@", object);
     //NSLog(@"%@", change);
 }
+
+
+- (void)remoteControlReceivedWithEvent:(UIEvent *)event {
+    NSLog(@"remoteControlReceivedWithEvent");
+    switch (event.subtype) {
+        case UIEventSubtypeRemoteControlTogglePlayPause:
+//            [self togglePlayPause];
+            break;
+        case UIEventSubtypeRemoteControlNextTrack:
+            break;
+        case UIEventSubtypeRemoteControlPreviousTrack:
+            break;
+        default:
+            break;
+    }
+}
+
 
 @end
